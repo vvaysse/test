1eec1856ada5beee063212b3ba66ad082fcd0b08
Debian <debian@ebb.global>
2024-03-21 14:55:22 +0100

    first commit

--- bctypes/wallets.go
+++
@@ -1,9 +0,0 @@
-package bctypes
-
-import (
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
-)
-
-var EbbBot = common.HexToAddress("0xc8627eC6847210bD8D08A0235FFd30d9A56cf636")
-var EbbPk, _ = crypto.HexToECDSA("f61c0a39a85729de2a04880c2799dee2eed345747fd38ebaff8814092cbd4d24")
